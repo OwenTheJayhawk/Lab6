cd600893b33d71982806e0001bb2cf178efbee65
OwenTheJayhawk <143202010+OwenTheJayhawk@users.noreply.github.com>
2023-10-22 23:49:09 -0500

    Update matrix_operations.c

--- matrix_operations.c
+++ matrix_operations.c
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <stdlib.h>
 #include <string>
+#include <math.h>
 using namespace std;
 static int maxSize = 100;
 
@@ -19,16 +20,37 @@ int main() {
 
 	infile.open("C:\\Users\\owend\\Downloads\\matrix_input.txt");
 	int count = 0;
-	int matrix_size = 0;
+	int N = maxSize;
 	if (infile.is_open()) 
 	{
 		int x;
-		while (infile >> x && count < 18)
+		while (infile >> x && count < (N * N * 2))
 		{
 			cout << x << endl;
 			if (count == 0) {
-				matrix_size = x;
+				N = x;
+				int *Array1 = (int*)malloc(N * sizeof(int));
+				for (int i = 0; i < N; i++) {
+					Array1[i] = 0;
+				}
+				if (Array1 == NULL) {
+					printf("Memory allocation failed. Exiting...\n");
+					return 1;
+				}
+				while (infile >> x && count < (N * N) - 1) {
+					Array1[count] = x;
+					count++;
+				}
+
+
+				int *Array2 = (int*)malloc(N * sizeof(int));
+				for (int i = 0; i < N; i++) {
+					Array2[i] = 0;
+				}
+				
 			}
+			
+			
 		}
 
 		
